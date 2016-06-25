d5864ac1bbe7f3476e24080442739849bc34b229
Marc Berchtold <marc.berchtold@hotmail.de>
2016-06-25 22:28:55 +0200

    Initial commit

---
+++ generateInfo_finishedGames.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by Marc Berchtold (shock2provide)
+ * Date: 23.06.2016
+ * Time: 18:58
+ */
+
+ini_set("auto_detect_line_endings", true);
+
+$logfile = file("testLog.log");
+analyseLogFile($logfile);
+
+function analyseLogFile($logfile)   {
+
+    $gameIsFinished = false;
+    $players = array();
+    $teams = array();
+
+    foreach($logfile as $line) {
+        $logFileObject = json_decode($line, true);
+
+        switch($logFileObject['event'])   {
+            case "log_start":
+                $logStartTimestamp = $logFileObject['unixTime'];
+                break;
+            case "player_status":
+                $players[$logFileObject['player']['uniqueId']] = $logFileObject['player'];
+                $players[$logFileObject['player']['uniqueId']]['kills'] = 0;
+                $players[$logFileObject['player']['uniqueId']]['assists'] = 0;
+                $players[$logFileObject['player']['uniqueId']]['deaths'] = 0;
+                $players[$logFileObject['player']['uniqueId']]['deaths'] = 0;
+                $players[$logFileObject['player']['uniqueId']]['headshots'] = 0;
+                $players[$logFileObject['player']['uniqueId']]['teamkills'] = 0;
+                break;
+            case "full_time":
+                foreach($logFileObject['teams'] as $team) {
+                    array_push($teams, $team);
+                }
+                $gameIsFinished = true;
+                break;
+            case "round_stats":
+                $players[$logFileObject['player']['uniqueId']]['kills'] += $logFileObject['kills'];
+                $players[$logFileObject['player']['uniqueId']]['assists'] += $logFileObject['assists'];
+                $players[$logFileObject['player']['uniqueId']]['deaths'] += $logFileObject['deaths'];
+                $players[$logFileObject['player']['uniqueId']]['headshots'] += $logFileObject['headshots'];
+                break;
+        }
+    }
+
+    print_r($players);
+}
